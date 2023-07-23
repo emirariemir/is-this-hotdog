7314bc063f6c1bd87ab32e970a212768771219bf
emir ari <emirariomatic@gmail.com>
2023-07-23 15:14:15 +0300

    Completed photo-picking functionality

--- IsThisHotdog/ViewController.swift
+++ IsThisHotdog/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let userTookedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imageView.image = userTookedImage
+        }
     }
 
-
+    @IBAction func takePhotoPressed(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+    }
+    
 }
 
